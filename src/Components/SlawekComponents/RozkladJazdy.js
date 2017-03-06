c574d30501e24f61ae6a1f043dc6934f2c9bcd63
Slawomir Grzebyk <grzebyk.slawomir@gmail.com>
2017-03-06 16:24:04 +0100

    Poprawiony Routing Pierwsza strona z Bootstrapem

--- src/Components/SlawekComponents/RozkladJazdy.js
+++ src/Components/SlawekComponents/RozkladJazdy.js
@@ -1,11 +1,36 @@
-/**
- * Created by oem on 2017-03-06.
- */
-import React from 'react';
+import React, {Component} from 'react';
+import {Grid, Row, Col} from "react-bootstrap"
 
+export class RozkladJazdy extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
 
-export const RozkladJazdy = () => (
+    render() {
+        const result = (
+            <Grid>
+                <Row>
+                    <Col md={8}>
+                        <h1>Rozklad Jazdy!</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi at cum, dicta
+                            eius et fugit illo in, ipsum itaque maiores modi nam quam quis ratione repellendus rerum
+                            soluta ullam.
+                        </p>
+                    </Col>
+                    <Col md={4}>
+                        <h1>Prawy Ekran</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi at cum, dicta
+                            eius et fugit illo in, ipsum itaque maiores modi nam quam quis ratione repellendus rerum
+                            soluta ullam.
+                        </p>
+                    </Col>
+                </Row>
+            </Grid>
+        );
+        return result;
+    }
+}
 
-    <h1>Rozklad Jazdy (StronaSlawka)</h1>
-
-) 
--- src/index.js
+++ src/index.js
@@ -9,11 +9,10 @@ import {RozkladJazdy} from './Components/SlawekComponents/RozkladJazdy'
 
 
 ReactDOM.render(
-
     <Router history={hashHistory}>
-      <Route path = "/" component = {App}/>
-      <Route path = "/rozklad-jazdy" component = {RozkladJazdy}/>
-      <Route path = "*" component = {Page404}/>
+      <Route path="/" component={App}/>
+      <Route path="/rozklad-jazdy" component={RozkladJazdy}/>
+      <Route path="*" component={Page404}/>
     </Router>
     ,
   document.getElementById('root')
