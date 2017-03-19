15b61023a7e5f4ec1ff963e70380bf407d4d9b9a
Aleks <otpusk.kiev@gmail.com>
2017-03-19 19:58:30 +0100

    zmiany strony

--- src/Components/ElementsPage/Detalis.js
+++ src/Components/ElementsPage/Detalis.js
@@ -5,28 +5,45 @@ import {Row, Col} from 'react-bootstrap';
 class Detalis extends React.Component{
     render() {
     return(
+        <div>
+           <div className="colorTitle"><p>Functions</p></div>
+        <div className="colorFunctions">
+            <row className="show-grid">
+                <Col md ={12} >
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Fusce lectus lacus, vestibulum non ligula a, elementum lobortis justo.
+                        Integer aliquet orci pharetra finibus tempor. Nunc vel est condimentum, maximus tellus ut, gravida tellus.
+                        Curabitur egestas mauris eu ex aliquet, iaculis posuere odio lobortis.
+                        Donec pulvinar orci ex, sit amet porttitor sapien cursus sed. Integer a pulvinar nulla.
+                        Suspendisse in enim purus.</p>
+                </Col>
+            </row>
+        </div>
+           <div className="colorTitle"><p>Details</p></div>
         <div className="colorDetalis">
-        <Row className="show-grid">
+        <row className="show-grid">
         <Col md ={6} >
-           <p><iframe width="400" height="220" src="https://www.youtube.com/embed/M9rYxYCaD9U">
+           <p><iframe width="390" height="220" src="https://www.youtube.com/embed/M9rYxYCaD9U">
            </iframe></p>
         </Col>
         <Col md={6} >
-            <p><iframe width="400" height="220" src="https://www.youtube.com/embed/AE-Y50eT1-o">
+            <p><iframe width="390" height="220" src="https://www.youtube.com/embed/AE-Y50eT1-o">
             </iframe></p>
         </Col>
-        </Row>
-        <Row className="show-grid">
+        </row>
+        <row className="show-grid">
             <Col md ={6} >
-            <p><iframe width="400" height="220" src="https://www.youtube.com/embed/M9rYxYCaD9U">
+            <p><iframe width="400" height="220" src="https://www.youtube.com/embed/sMzdCfCRFG4">
             </iframe></p>
             </Col>
             <Col md={6} >
-            <p><iframe width="400" height="220" src="https://www.youtube.com/embed/AE-Y50eT1-o">
+            <p><iframe width="400" height="220" src="https://www.youtube.com/embed/6Fw6EMpkvQA">
             </iframe></p>
             </Col>
-       </Row>
-     </div>);
+       </row>
+          </div>
+        </div>
+        );
 
  };
 }
