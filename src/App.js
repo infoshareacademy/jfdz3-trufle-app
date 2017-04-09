8e60475948c207ab02bc5c83d936ad7d3e98cc31
Aleks <otpusk.kiev@gmail.com>
2017-04-09 16:47:37 +0200

    Merge branch 'master' of https://github.com/infoshareacademy/jfdz3-trufle-app into Leszek
    # Conflicts:
    #	.idea/workspace.xml

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Menu from './Components/ElementsPage/Menu.js';
 import Sliders from './Components/ElementsPage/Sliders.js';
 import Detalis from './Components/ElementsPage/Main.js';
 import Footer from './Components/ElementsPage/Footer.js';
+import Forms from './Components/ElementsPage/Forms.js';
 
 class App extends Component {
 
@@ -12,10 +13,15 @@ class App extends Component {
     return (
       <Grid fluid>
         <Row className="show-grid">
-          <Col className="App_nav" xs={12} md={12}>
+          <Col className="App_nav" xs={12} md={12} lg={12}>
             <Menu />
           </Col>
         </Row>
+          <Row className="show-grid">
+              <Col className="Forms" xs={12} md={12} lg={12}>
+                  <Forms />
+              </Col>
+          </Row>
         <Row className="show-grid App-change">
           <Sliders />
         </Row>
--- src/Components/ElementsPage/Menu.js
+++ src/Components/ElementsPage/Menu.js
@@ -1,46 +1,26 @@
 import React from 'react';
 import './Menu.css';
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
-import {Form, FormGroup, ControlLabel, FormControl, Button, Col} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
 import logo from './images/planeta.gif';
 
 class Menu extends React.Component {
   render() {
     return (
-
       <Navbar fluid>
-        <Col md={6}>
+        <Col md={12} lg={12}>
           <Navbar.Header>
             <Navbar.Brand>
               <a className='logo_images' href="#"><img src={logo} alt="logo"/></a>
               <a className='logo_name' href="#">Trufle</a>
             </Navbar.Brand>
-          </Navbar.Header>
           <Nav>
             <NavItem eventKey={1} href="#" className='button_menu'>Functions</NavItem>
             <NavItem eventKey={2} href="#" className='button_menu'>Team</NavItem>
             <NavItem eventKey={3} href="#" className='button_menu'>Details</NavItem>
             <NavItem eventKey={4} href="#" className='button_menu'>Newsletter</NavItem>
           </Nav>
-        </Col>
-        <Col md={6} className="form-menu">
-          <Form inline>
-            <FormGroup controlId="formInlineName">
-              <ControlLabel>Name</ControlLabel>
-              {' '}
-              <FormControl type="text" placeholder="Jane Doe"/>
-            </FormGroup>
-            {' '}
-            <FormGroup controlId="formInlineEmail">
-              <ControlLabel>Email</ControlLabel>
-              {' '}
-              <FormControl type="email" placeholder="jane.doe@example.com"/>
-            </FormGroup>
-            {' '}
-            <Button type="submit" bsStyle="primary" className="button_form">
-              Send invitation
-            </Button>
-          </Form>
+          </Navbar.Header>
         </Col>
       </Navbar>
     )
